002518edaa7d372dbd71fe00cdab79ebbfa2a5e3
cfmusoles <c.f.musoles@sheffield.ac.uk>
2019-06-11 14:08:36 +0100

    testing hypergraph modelling of synapses

--- test_experiment.py
+++ test_experiment.py
@@ -60,7 +60,7 @@ run_experiment() {
 	do
 		#aprun -n $P $APP_NAME -n $EXPERIMENT_NAME -c $COMM_PATTERN -p $DISTRIBUTION -s $SEED -k 500 -f 1000 -t 150 -m "cm" -i 24 -b $BM_FILE
 		sleep 1
-		aprun -n $P $APP_NAME -n $EXPERIMENT_NAME -c $COMM_PATTERN -p $DISTRIBUTION -s $SEED -k 200 -f 200 -t 150 -m "mvc" -i 24 -b $BM_FILE
+		aprun -n $P $APP_NAME -n $EXPERIMENT_NAME -c $COMM_PATTERN -p $DISTRIBUTION -s $SEED -k 100 -f 150 -t 150 -m "mvc" -i 24 -b $BM_FILE
 		sleep 1
 	done
 }
@@ -68,7 +68,7 @@ run_experiment() {
 for r in $(seq 1 $TEST_ITERATIONS)
 do
 	SEED=$RANDOM
-	run_experiment $PROCESSES "roundrobin" "nbx" $SEED 
+	#run_experiment $PROCESSES "roundrobin" "nbx" $SEED 
 	run_experiment $PROCESSES "prawP" "nbx" $SEED 
 	run_experiment $PROCESSES "hypergraphPartitioning" "nbx" $SEED 
 done
