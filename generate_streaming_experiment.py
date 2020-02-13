5810ab37a0684c1e356bff32999f308f138abddb
cfmusoles <c.f.musoles@sheffield.ac.uk>
2020-02-13 11:50:01 +0000

    added hyperPRAW experiment batch files

---
+++ generate_streaming_experiment.py
@@ -0,0 +1,113 @@
+# Create ARCHER job files based on parameters passed
+# This experiment focuses on comparing runtime impact on SNN simulations when using alternative partitioning methods to distribute workloads:
+#	- zoltan
+#	- hyperPRAW (architecture aware streaming)
+# Both approaches are tested with and without previous neuron activity information
+
+
+import sys
+import math
+
+#job templates
+template_1 = '''#!/bin/bash --login
+
+# name of the job
+#PBS -N '''
+template_2 = '''
+# how many nodes
+#PBS -l select='''
+template_3=''':bigmem='''
+template_4='''
+# walltime
+#PBS -l walltime=24:00:0
+# budget code
+#PBS -A e582
+
+APP_NAME="distSim"
+PRUNE_COLUMN=4 # propagation time (sync time + idle)
+ITERATIONS=2
+REPETITIONS=1
+PROCESSES='''
+template_5='''
+EXPERIMENT_NAME='''
+template_6='''
+# This shifts to the directory that you submitted the job from
+cd $PBS_O_WORKDIR
+
+# bandwidth probing parameters
+SIZE=512
+ITERATIONS=20
+WINDOW=10
+#renaming is necessary to avoid clashes between simultaneous jobs
+ORIGINAL_BM_FILE="results_mpi_send_bandwidth_"$PROCESSES
+aprun -n $PROCESSES mpi_perf $SIZE $ITERATIONS $WINDOW
+for p in $(seq 1 10)
+do
+	FILENAME="results_mpi_send_bandwidth_"$p"_"$PROCESSES
+	if [ ! -f $FILENAME ]; then
+	    BM_FILE="results_mpi_send_bandwidth_"$p"_"$PROCESSES
+	    break
+	fi
+done
+
+mv $ORIGINAL_BM_FILE $BM_FILE
+
+#two args:
+# $1 > distribution algorithm used by simulator
+# $2 > if results should be pruned
+run_experiment() {
+	P="$1"
+	PARTITIONING="$2"
+	COMM_PATTERN="$3"
+	SEED="$4"
+	ACTIVITY_FILE=$EXPERIMENT_NAME"_"$PARTITIONING"_"$COMM_PATTERN"__"$P"_neuron_activity"
+
+	for i in $(seq 1 $REPETITIONS)
+	do
+		aprun -n $P $APP_NAME -n $EXPERIMENT_NAME -c $COMM_PATTERN -p $PARTITIONING -s $SEED -k 1000 -f 160 -t 700 -m "mvc" -i 24 -b $BM_FILE
+		aprun -n $P $APP_NAME -n $EXPERIMENT_NAME"_neuron_activity" -c $COMM_PATTERN -p $PARTITIONING -s $SEED -k 1000 -f 160 -t 700 -m "mvc" -i 24 -b $BM_FILE -w $ACTIVITY_FILE
+		#aprun -n $P $APP_NAME -n $EXPERIMENT_NAME -c $COMM_PATTERN -p $PARTITIONING -s $SEED -k 500 -f 1000 -t 750 -m "cm" -i 24 -b $BM_FILE
+		sleep 1
+	done
+}
+
+
+
+
+for r in $(seq 1 $ITERATIONS)
+do
+	SEED=$RANDOM
+	run_experiment $PROCESSES "prawV" "nbx" $SEED
+	run_experiment $PROCESSES "hypergraphPartitioning" "nbx" $SEED
+	
+done
+
+'''
+
+
+if len(sys.argv) < 6:
+	print("Input error: usage -> python generate_archer_job.py jobName min_processes num_experiments geometric_step big_mem[true|false]")
+	exit()
+
+test_name = sys.argv[1]
+min_processes = int(sys.argv[2])
+num_experiments = int(sys.argv[3])
+geometric_step = int(sys.argv[4])
+big_mem = (sys.argv[5] == "true" or sys.argv[5] == "True")
+
+process_counts = [min_processes * geometric_step ** (n-1) for n in range (1, num_experiments+1)]
+print("Generating experiments")
+print(process_counts)
+
+for p in process_counts:
+	nodes = max(int(math.ceil(p / 24)),1)
+	writebuffer = open("archer_job_" + test_name + "_" + str(p) + ".sh",'w')
+	writebuffer.write(template_1 + test_name)
+	writebuffer.write(template_2 + str(nodes))
+	writebuffer.write(template_3 + str(big_mem).lower())
+	writebuffer.write(template_4 + str(p))
+	writebuffer.write(template_5 + test_name)
+	writebuffer.write(template_6)
+
+
+
--- include/HyperPRAWVertexPartitioning.h
+++ include/HyperPRAWVertexPartitioning.h
@@ -17,67 +17,103 @@
 class HyperPRAWVertexPartitioning : public Partitioning {
 public:
 	
-	HyperPRAWVertexPartitioning(std::vector<Population*>* pops, int population_size, char* comm_bandwidth_file, bool parallel, bool useBandwidth) : Partitioning(pops,population_size) {
+	HyperPRAWVertexPartitioning(std::vector<Population*>* pops, int population_size, char* comm_bandwidth_file) : Partitioning(pops,population_size) {
 		comm_bandwidth_filename = comm_bandwidth_file;
-        isParallel = parallel;
-        use_bandwidth_file = useBandwidth;
 	}
 	virtual ~HyperPRAWVertexPartitioning() {}
 	
 	virtual void perform_partitioning(Model* model, int partitions, int process_id, std::vector<int>* previous_activity) {
-		int max_iterations = 100;
+		// fixed parameters for HyperPRAW
+        int max_iterations = 100;
         float imbalance_tolerance = 1.2f;
-        
+        int num_streams = partitions;
+        bool local_parallel_update_only = false;
+        int sync_batch_size = 1;
+        bool input_order_round_robin = true;
+        float lambda = 0.8;
+        bool save_partitioning_history = true;
+
         if(partitions <= 1) {
 			PRINTF("Partitioning not required\n");
 			return;
 		}
 
-        // PREP DATA STRUCTURES
-        // initialise vertex weight values
-        int* vtx_wgt = (int*)calloc(model->population_size,sizeof(int));
-        for(int ii =0; ii < model->population_size; ii++) {
-            vtx_wgt[ii] = 1;
-        }
-        // Zoltan does consider this balance, so needs to be a fair comparison
-        for(int ii=0; ii < model->population_size; ii++) {
-            for(int jj=0; jj < model->interconnections_size[ii]; jj++) {
-                vtx_wgt[abs(model->interconnections[ii][jj])] += 1;
+        // only active partitions (streams) should bother with it
+        // the others will wait for the result and synchronise
+
+        MPI_Comm partitioning_comm;
+
+        // Create MPI communicator group (of only max_number_processes)
+        // Only those processes will work towards partitioning
+        MPI_Comm_split(MPI_COMM_WORLD,
+                        process_id < num_streams ? 1 : 0, // this is the colour, is used to match the processes to the new group
+                        process_id,                                         // this is the key, used to generate new rank
+                        &partitioning_comm);                               // the subgroup generated
+        
+        int num_processes;
+        MPI_Comm_size(partitioning_comm,&num_processes); 
+
+        bool is_active = process_id < num_streams;
+
+        if(is_active) {
+            // PREP DATA STRUCTURES
+            // initialise vertex weight values
+            int* vtx_wgt = (int*)calloc(model->population_size,sizeof(int));
+            for(int ii =0; ii < model->population_size; ii++) {
+                vtx_wgt[ii] += 1;
+                // Zoltan does consider this balance, so needs to be a fair comparison
+                for(int jj=0; jj < model->interconnections_size[ii]; jj++) {
+                    vtx_wgt[abs(model->interconnections[ii][jj])] += 1;
+                }
             }
-        }
 
-        // p2p communication cost estimates from file
-        double** comm_cost_matrix = (double**)malloc(sizeof(double*) * partitions);
-        for(int ii=0; ii < partitions; ii++) {
-            comm_cost_matrix[ii] = (double*)calloc(partitions,sizeof(double));
-        }
+            // p2p communication cost estimates from file
+            double** comm_cost_matrix = (double**)malloc(sizeof(double*) * partitions);
+            for(int ii=0; ii < partitions; ii++) {
+                comm_cost_matrix[ii] = (double*)calloc(partitions,sizeof(double));
+            }
+            // communication cost matrix
+            PRAW::get_comm_cost_matrix_from_bandwidth(comm_bandwidth_filename,comm_cost_matrix,partitions,false);  
 
-        if(use_bandwidth_file)
-            PRAW::get_comm_cost_matrix_from_bandwidth(comm_bandwidth_filename,comm_cost_matrix,partitions,false);
-        else
-            PRAW::get_comm_cost_matrix_from_bandwidth(NULL,comm_cost_matrix,partitions,false);
-        
-        
-        // 1 Save current model to hMETIS format file (only one process needs to do this)
-        //      Header: num_hyperedges num_vertices
-        //      each line after that: a hyperedge which lists the contained vertices
-        // 2 Pass on the info to PRAW::ParallelIndependentRestreamingPartitioning (or sequential version) to get partitioning
-        // 3 Make sure all partitions have the final partitioning
-        // 4 Clean up unused datastructures
+            // create hMETIS file (distributed format)
+            // each line represents a vertex (neuron) and the ids of the hyperedges (synapsys) that contain it
+            // user model->interconnections_size and model->interconnections[][] to build file
 
-        /////////////////////////////////////////
-        //for PRAW::ParallelHyperedgePartitioning
-        /////////////////////////////////////////
-        // temporary hMETIS file
-        /*std::string hgraph_file = "model_";
-        char str_int[16];
-        sprintf(str_int,"%i",partitions);
-        hgraph_file += str_int;
-        hgraph_file += ".hgr";
-        // MASTER NODE creates hMETIS file
-        if(process_id == 0) {
-            // generate hMETIS file
-            // store model in file
+            std::string hgraph_file = "model_";
+            hgraph_file += "_";
+            char str_int[16];
+            sprintf(str_int,"%i",num_processes);
+            hgraph_file += str_int;
+            hgraph_file += "_";
+            sprintf(str_int,"%i",process_id);
+            hgraph_file += str_int;
+            hgraph_file += "_";
+            sprintf(str_int,"%i",partitions);
+            hgraph_file += str_int;
+            hgraph_file += ".hgr";
+
+            int local_stream_size = model->population_size / num_processes + ((model->population_size % num_processes > process_id) ? 1 : 0);
+            std::vector<std::vector<int> > hedge_ptr(local_stream_size);
+            int counter = 1; // indices start in 1
+            for(int ii=0; ii < model->population_size; ii++) {
+                // account for presynaptic neuron, as it belongs to the hyperedge too
+                int pre_syn_vertex = ii;
+                if(pre_syn_vertex % num_processes == process_id) {
+                    hedge_ptr[pre_syn_vertex / num_processes].push_back(counter);
+                }
+                // all postsynaptic connections also belong to the same hyperedge as pre synaptic neuron
+                for(int jj=0; jj < model->interconnections_size[pre_syn_vertex]; jj++) {
+                    int post_synaptic_vertex = abs(model->interconnections[pre_syn_vertex][jj]);
+                    if(post_synaptic_vertex % num_processes == process_id) {
+                        hedge_ptr[post_synaptic_vertex / num_processes].push_back(counter);
+                    }
+                }
+                counter++;            
+            }
+
+            printf("%i: %li\n",process_id,hedge_ptr.size());
+
+            // create hMETIS (distributed format) file
             PRINTF("%i: Storing model in file %s\n",process_id,hgraph_file.c_str());
             FILE *fp = fopen(hgraph_file.c_str(), "w+");
             
@@ -86,134 +122,45 @@ public:
             fprintf(fp,"%lu %lu",model->population_size,model->population_size);
             fprintf(fp,"\n");
 
-            // write connectivity per neuron id
-            for(int ii=0; ii < model->population_size; ii++) {
-                // write presynaptic neuron as first neuron
-                fprintf(fp,"%i ",ii + 1);
-                // write all connections from ii
-                for(int jj=0; jj < model->interconnections_size[ii]; jj++) {
-                    fprintf(fp,"%i ",abs(model->interconnections[ii][jj]) + 1);
+            for(int ii=0; ii < hedge_ptr.size(); ii++) {
+                for(int he=0; he < hedge_ptr[ii].size(); he++) {
+                    fprintf(fp,"%i ",hedge_ptr[ii][he]);
                 }
-                
                 fprintf(fp,"\n");
+                
             }
             fclose(fp);
+            //////////////////////////////////////////////////////
 
-        }
-        ////////////////////////////////////////
-         */
-
-
-        /////////////////////////////////////////
-        //for PRAW::ParallelHDRF (rHDRF)
-        /////////////////////////////////////////
-        std::string hgraph_file = "model_";
-        hgraph_file += "_";
-        char str_int[16];
-        sprintf(str_int,"%i",partitions);
-        hgraph_file += str_int;
-        hgraph_file += "_";
-        sprintf(str_int,"%i",process_id);
-        hgraph_file += str_int;
-        hgraph_file += ".hgr";
-        // create hMETIS file (distributed format)
-        // each line represents a vertex
-        // the contents of the line are the hyperedge ids that vertex belongs to
-        // generate intermediate datastructure with list of hedge ids
-
-        std::vector<std::vector<int> > hedge_ptr(ceil((float)model->population_size / partitions));
-        // read reminder of file (one line per hyperedge)
-        int counter = 1;
-        for(int ii=0; ii < model->population_size; ii++) {
-            // account for presynaptic neuron, as it belongs to the hyperedge too
-            int pre_syn_vertex = ii;
-            if(pre_syn_vertex % partitions == process_id) {
-                hedge_ptr[pre_syn_vertex / partitions].push_back(counter);
-            }
-            // all postsynaptic connections
-            for(int jj=0; jj < model->interconnections_size[ii]; jj++) {
-                int vertex_id = abs(model->interconnections[ii][jj]);
-                if(vertex_id % partitions == process_id) {
-                    hedge_ptr[vertex_id / partitions].push_back(counter);
-                }
-            }
-            counter++;            
-        }
-
-        // create hMETIS (distributed format) file
-        PRINTF("%i: Storing model in file %s\n",process_id,hgraph_file.c_str());
-        FILE *fp = fopen(hgraph_file.c_str(), "w+");
-        
-        // write header: NUM_HYPEREDGES NUM_VERTICES
-        //  num hyperedges == number of vertices, since each hyperedge represents a presynaptic neuron and all its connecting post synaptic neighbours
-        fprintf(fp,"%lu %lu",model->population_size,model->population_size);
-        fprintf(fp,"\n");
-
-        for(int ii=0; ii < hedge_ptr.size(); ii++) {
-            for(int he=0; he < hedge_ptr[ii].size(); he++) {
-                fprintf(fp,"%i ",hedge_ptr[ii][he]);
-            }
-            fprintf(fp,"\n");
-            
-        }
-        fclose(fp);
-        //////////////////////////////////////////////////////
-
-
-
-        MPI_Barrier(MPI_COMM_WORLD); 
-
-        // define algorithmic parameters
-        float ta_refine = 0.95f;
-        int stopping_condition = 2;
-        std::string filename = hgraph_file;
-        if(isParallel) {
+            // Perform partitioning amongst the streams
+            std::string filename = hgraph_file;
             filename += "_prawParallel";
-            char experiment_name[filename.length() + 1]; 
+            char experiment_name[filename.length() + 1];
             strcpy(experiment_name, filename.c_str()); 
-            //PRAW::ParallelHyperedgePartitioning(experiment_name,partitioning,comm_cost_matrix, hgraph_file.c_str(),vtx_wgt,max_iterations, imbalance_tolerance,ta_refine,true,stopping_condition,false);
-            // alternative method (based on Alistairh streaming, only one vertex with all its hyperedges at a time)
-            //PRAW::ParallelHyperedgePartitioning_he_stream(experiment_name,partitioning,comm_cost_matrix, model->population_size,&he_stream,vtx_wgt,max_iterations, imbalance_tolerance,false);
-            PRAW::ParallelHDRF(experiment_name,partitioning, comm_cost_matrix, hgraph_file, vtx_wgt, max_iterations, imbalance_tolerance, true);
-        } else {
-            filename += "_prawSequential";
-            char experiment_name[filename.length() + 1]; 
-            strcpy(experiment_name, filename.c_str()); 
-            PRAW::SequentialVertexPartitioning(experiment_name,partitioning,partitions,comm_cost_matrix, hgraph_file.c_str(),vtx_wgt,max_iterations, imbalance_tolerance,ta_refine,true,stopping_condition, false);
-        }
+            int iterations = PRAW::ParallelHDRF(experiment_name, partitioning, partitions, partitioning_comm, comm_cost_matrix, hgraph_file.c_str(), vtx_wgt, max_iterations, imbalance_tolerance,local_parallel_update_only,sync_batch_size,input_order_round_robin,lambda,save_partitioning_history,&previous_activity->at(0));
 
-        if(process_id == 0) {
-            if(!use_bandwidth_file && comm_bandwidth_filename != NULL)
-                PRAW::get_comm_cost_matrix_from_bandwidth(comm_bandwidth_filename,comm_cost_matrix,partitions,false);
-                
-            //PRAW::storePartitionStats(filename,partitioning,partitions,model->population_size,&hyperedges,&hedge_ptr,vtx_wgt,comm_cost_matrix);
-            // Use PRAW::getVertexCentricPartitionStatsFromFile instead and record results manually if needed
-        }
-
-        free(vtx_wgt);
-        for(int ii=0; ii < partitions; ii++) {
-            free(comm_cost_matrix[ii]);
-        }
-        free(comm_cost_matrix);
-
-        // wait for all before deleting the hgraph file
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // remove hMETIS files
-        /*if(process_id == 0) {
-            // remove graph file
+            // clean up files
             if( remove(hgraph_file.c_str()) != 0 )
                 printf( "Error deleting temporary hgraph file %s\n",hgraph_file.c_str() );
-        }*/
-        if( remove(hgraph_file.c_str()) != 0 )
-            printf( "Error deleting temporary hgraph file %s\n",hgraph_file.c_str() );
+            
+            // clean memory
+            free(vtx_wgt);
+            for(int ii=0; ii < partitions; ii++) {
+                free(comm_cost_matrix[ii]);
+            }
+            free(comm_cost_matrix);
 
+        }
+
+        // synchronise partitioning with all processes
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        // broadcast results to not participating processes
+        MPI_Bcast(partitioning, model->population_size, MPI_LONG, 0,MPI_COMM_WORLD);
 	}
 
 private:
     char* comm_bandwidth_filename = NULL;
-    bool isParallel = false;
-    bool use_bandwidth_file = false;
 };
 
 
